e4fcd396660f2038c10461ec76910f6bd5bed59d
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-18 05:01:54 -0400

    feat(login): add login button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function Login() {
+  function handleLogin() {}
+
   return (
     <div>
       <h1>Login</h1>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
