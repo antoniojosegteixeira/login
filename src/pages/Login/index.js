d009d81384fd76bc578c8a750893f4697fac2799
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-18 07:44:19 -0400

    feat: call function on login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import { useAuth } from "../../contexts/auth";
 
 export default function Login() {
-  const { user } = useAuth();
+  const { user, login } = useAuth();
   function handleLogin() {
-    console.log(user);
+    login();
   }
 
   return (
     <div>
       <h1>Login</h1>
+      <span>Username: {user.userName}</span>
+      <hr />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
