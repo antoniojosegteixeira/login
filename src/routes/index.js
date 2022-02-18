840e463d270e369e3094a7f518db9524f60c24dd
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-18 05:08:56 -0400

    feat(route): add index component

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import PrivateRoutes from "./PrivateRoutes";
+import PublicRoutes from "./PublicRoutes";
+
+export default function Routes() {
+  return <PublicRoutes />;
+}
