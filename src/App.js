6846f22a93b19c223f986e9e90e516266dbc3bd8
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-18 19:47:11 -0400

    feat: create redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Routes from "./routes";
 import { AuthProvider } from "./contexts/auth";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <Provider>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Provider>
   );
 }
 
