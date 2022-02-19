ed46388b917d08beb40d9587489ce32b52f3195f
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-18 22:33:40 -0400

    feat: add login reducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,13 +1,22 @@
 const initialState = {
-  name: "João",
-  id: "",
+  userName: "",
+  userId: "",
   token: "",
+  isSignedIn: false,
 };
 
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case "CHANGE_USER_NAME":
       return { ...state, name: action.payload };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        userName: action.payload.userName,
+        userId: action.payload.userId,
+        token: action.payload.token,
+        isSignedIn: true,
+      };
     default:
       return state;
   }
